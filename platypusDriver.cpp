1462a4f4b0877ff99074a41e4fb19bd54c09ab46
cyreilv7 <shoiiru3@gmail.com>
2022-03-10 20:59:15 -0800

    Label outputs for testing

--- platypusDriver.cpp
+++ platypusDriver.cpp
@@ -13,18 +13,24 @@ int main()
 {
     Platypus p1;
     Platypus p2("Champ", 5.0, 0, 'M');
+
+    cout << "************* TESTING CONSTRUCTORS *************" << endl;
     p1.print();
     p2.print();
     
+    cout << "************* TESTING HATCH() *************" << endl;
     p1.hatch();
     p1.print();
     
+    cout << "************* TESTING EAT() *************" << endl;
     p1.eat();
     p1.print(); // expected: weight changes
     
+    cout << "************* TESTING AGEME() *************" << endl;
     p1.ageMe();
     p1.print(); // expected new age: 1
     
+    cout << "************* TESTING FIGHT() *************" << endl;
     p1.fight(p2);// one is dead
     p1.print();
     p2.print();
