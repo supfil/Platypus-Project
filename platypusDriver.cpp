5f271db16bd9429192942e1cce70067aa1d0e289
Filipp <75974434+supfil@users.noreply.github.com>
2022-03-14 22:16:13 -0700

    made some functions
    Platypus intro();
    char menu();
    void eatingFood(Platypus$);
    void grow(Platypus$);
    void platypusFight(Platypus$);
    void quit(char&);

--- platypusDriver.cpp
+++ platypusDriver.cpp
@@ -9,8 +9,16 @@
 #include "platypus.h"
 using namespace std;
 
+Platypus intro();   //this will ask the user for info about thier platypus
+char menu(); // it will allow the user to pick options for what to do
+void eatingFood(Platypus$);//makes the users Platypus use the eat()
+void grow(Platypus$);// this makes the user's platypus grow
+void platypusFight(Platypus$);// this makes the user's platypus fight
+void quit(char&); //  this makes the program stop
+
 int main()
 {
+    /*
     Platypus p1;
     Platypus p2("Champ", 5.0, 0, 'M');
 
@@ -34,16 +42,77 @@ int main()
     p1.fight(p2);// one is dead
     p1.print();
     p2.print();
+    */
 
-    /*
-    char repeat;
+    
+    char repeat,
+         decision;
+    
+    //  create a platypus from user input along with the intro
+    Platypus p1;
+    p1 = intro();
+    
     while(true)
     {
+        //  options for the user
+        decision = menu();
         
-        if (repeat == 'y')
+        //  the menu decides what to do
+        switch (decision) {
+            case 'e':
+                eatingFood(p1);
+                break;
+            case 'g':
+                grow(p1);
+                break;
+            case 'f':
+                platypusFight(p1);
+                break;
+            case 'q':
+                quit(repeat);
+                break;
+            default:
+                cout << "\nSorry that isn't an option, try again\n";
+                break;
+        }
+        
+        if (repeat == 'y'||repeat == 'Y')
             break;
     }
-     */
+    
     
     return 0;
+}
+
+Platypus intro()//this will ask the user for info about thier platypus
+{
+    
+}
+
+char menu() // it will allow the user to pick options for what to do
+{
+    char choice;
+    cout << "";
+    cin >> choice;
+    return choice;
+}
+
+void eatingFood(Platypus$)//makes the users Platypus use the eat()
+{
+    
+}
+
+void grow(Platypus$)// this makes the user's platypus grow
+{
+    
+}
+
+void platypusFight(Platypus$)// this makes the user's platypus fight
+{
+    
+}
+
+void quit(char&) //  this makes the program stop
+{
+    
 }
